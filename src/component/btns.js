3068ea794c99bbfa2e79d729926dce38801e3f9b
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-08-04 12:50:36 +0430

    update bookslice.js

--- src/component/btns.js
+++ src/component/btns.js
@@ -1,16 +1,38 @@
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { apiErase } from '../redux/books/booksSlice';
 
 function ActionButtons({ book }) {
   const dispatch = useDispatch();
+  const [showEditForm, setShowEditForm] = useState(false);
+
+  const handleRemoveBook = () => {
+    dispatch(apiErase(book.item_id));
+  };
+
+  const handleCloseEditForm = () => {
+    setShowEditForm(false);
+  };
+
   return (
-    <div className="buttonCont">
-      <button type="button">Comments</button>
+    <div className="action-buttons">
+      <button type="button" className="comments-btn">Comments</button>
       |
-      <button type="button" className="btnAcc" onClick={() => dispatch(apiErase(book.item_id))}>Remove</button>
+      <button type="button" className="remove-button" onClick={handleRemoveBook}>Remove</button>
       |
-      <button type="button" className="btnAcc">Edit</button>
+      <button type="button" className="edit-button">Edit</button>
+      {showEditForm && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Edit Book</h2>
+            <input type="text" placeholder="New Title" />
+            <input type="text" placeholder="New Author" />
+            <button type="button" onClick={handleCloseEditForm}>Cancel</button>
+            <button type="button">Save</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
@@ -18,6 +40,9 @@ function ActionButtons({ book }) {
 ActionButtons.propTypes = {
   book: PropTypes.shape({
     item_id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
   }).isRequired,
 };
 
